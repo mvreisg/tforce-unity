7a9dfd247306dbfa99f83ee3eb7126275baa349b
mvreisg <marcus_viniciusreis@hotmail.com>
2020-12-14 21:25:52 -0300

    First Commit 14122020

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    private Transform backPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        backPoint = GameObject.Find("BackPoint").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < backPoint.position.x)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
